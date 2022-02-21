87418ee65d2e11912d693dde8d242a76f39e7e54
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-21 17:43:06 +0100

    [React Basics] 35 Add basic CSS styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>21-02-2022</div>
-      <div>
+      <div className='expense-item__description'>
         <h2>Car Insurance</h2>
-        <div>$310.45</div>
+        <div className='expense-item__price'>$310.45</div>
       </div>
     </div>
   );
