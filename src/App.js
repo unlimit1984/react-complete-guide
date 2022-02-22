0b365ef7aaacbfe388715a6d40cf6de0ee34b084
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-22 17:01:23 +0100

    [React Basics] 43 Organize component files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'; // OPTION 2
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
