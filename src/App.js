2c86283ceab58688a70751cc792be00fcf8a989f
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-22 17:06:39 +0100

    [React Basics] 44 Use arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'; // OPTION 2
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -39,6 +39,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
