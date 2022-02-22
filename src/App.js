5d2bd153af06ee9941b30316ef8929654a395a4c
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-22 16:50:39 +0100

    [React Basics] 42 A Closer Look at JSX
    - Add react import with syntax sugar option

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'; // OPTION 2
 import Expenses from './components/Expenses';
 
 function App() {
@@ -22,6 +23,16 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // OPTION 2
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  // OPTION 1
   return (
     <div>
       <h2>Let's get started!</h2>
