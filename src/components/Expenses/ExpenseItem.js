aa6350ce00edc0f44e1f2f9fcbcfb3e177e4c990
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-23 19:12:33 +0100

    [React State&Events] 49 Working with State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // function clickHandler() {
-  //   console.log('Clicked!');
-  // }
+  const [title, setTitle] = useState(props.title);
 
+  // function clickHandler() { }
   const clickHandler = () => {
-    console.log('Clicked!');
+    setTitle('Updated!');
+    console.log(title);
   };
+
+  console.log('On Rendering');
+  console.log(title);
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
 
