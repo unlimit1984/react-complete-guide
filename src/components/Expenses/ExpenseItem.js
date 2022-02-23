532058cc44540f78c722bb924ead7d1fefbd450c
unlimit1984 <unlimit_rmc@mail.ru>
2022-02-23 17:44:08 +0100

    [React State&Events] 47 Add first event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,14 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // function clickHandler() {
+  //   console.log('Clicked!');
+  // }
+
+  const clickHandler = () => {
+    console.log('Clicked!');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -10,6 +18,30 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+
+      {/* OPTION 1 */}
+      {/* <button onClick={() => console.log('Clicked!')}>Change title</button> */}
+
+      {/* OPTION 2 */}
+      {/* <button
+        onClick={() => {
+          console.log('Clicked!');
+        }}
+      >
+        Change title
+      </button> */}
+
+      {/* OPTION 3 */}
+      {/* <button
+        onClick={function () {
+          console.log('Clicked!');
+        }}
+      >
+        Change title
+      </button> */}
+
+      {/* OPTION 4 */}
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
