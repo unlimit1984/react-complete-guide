6f5a49fe216f575092c84bb6133ed145b0a68bcf
unlimit1984 <unlimit_rmc@mail.ru>
2022-03-21 23:14:58 +0100

    [08-My-UserForm] 89_3 Add dark background

--- 08-my-user-form-app/src/components/Modal/Modal.js
+++ 08-my-user-form-app/src/components/Modal/Modal.js
@@ -15,13 +15,23 @@ const Modal = (props) => {
   };
 
   return (
-    <div className={styles.modal}>
-      <p>Invalid input</p>
-      {data.username && data.username.length > 0 && <p>{data.username}</p>}
-      {data.age && data.age.length > 0 && <p>{data.age}</p>}
-      <button type="button" onClick={closeHandler}>
-        Okay
-      </button>
+    <div>
+      <div className={styles['modal-background']} onClick={closeHandler}>
+        <p>Text</p>
+        <div className={styles.inner}></div>
+        <p>Text</p>
+      </div>
+
+      <div className={styles['modal-content']}>
+        <div className={styles.header}>Invalid input</div>
+        <div className={styles.content}>
+          {data.username && data.username.length > 0 && <p>{data.username}</p>}
+          {data.age && data.age.length > 0 && <p>{data.age}</p>}
+          <button type="button" onClick={closeHandler}>
+            Okay
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
