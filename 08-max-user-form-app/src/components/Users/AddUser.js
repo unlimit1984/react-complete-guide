12a88f21c750c818d9c887eeadb138c6eebe1768
unlimit1984 <unlimit_rmc@mail.ru>
2022-03-25 22:00:22 +0100

    [08-Max-UserForm] 92 Add a re-usable Button
    - Also provide .prettier

--- 08-max-user-form-app/src/components/Users/AddUser.js
+++ 08-max-user-form-app/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
-import Card from "../UI/Card";
-import classes from "./AddUser.module.css";
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -13,7 +14,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
