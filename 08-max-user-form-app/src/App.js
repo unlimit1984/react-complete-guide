226942b02d22779e2a823a80ab809032c3d831da
unlimit1984 <unlimit_rmc@mail.ru>
2022-03-27 22:27:43 +0200

    [09] 103 Add a Fragment

--- 08-max-user-form-app/src/App.js
+++ 08-max-user-form-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Wrapper from './components/Helpers/Wrapper';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
@@ -8,15 +9,33 @@ function App() {
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => [
       ...prevUsersList,
-      { name: uName, age: uAge, id: Math.random().toString() }
+      { name: uName, age: uAge, id: Math.random().toString() },
     ]);
   };
 
   return (
-    <div>
+    // OPTION 1
+    // <div>
+    //   <AddUser onAddUser={addUserHandler} />
+    //   <UsersList users={usersList} />
+    // </div>
+
+    // OPTION 2
+    // <Wrapper>
+    //   <AddUser onAddUser={addUserHandler} />
+    //   <UsersList users={usersList} />
+    // </Wrapper>
+
+    // OPTION 3
+    // <React.Fragment>
+    //   <AddUser onAddUser={addUserHandler} />
+    //   <UsersList users={usersList} />
+    // </React.Fragment>
+    // OPTION 4 -> alternative to <React.Fragment>
+    <>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </>
   );
 }
 
