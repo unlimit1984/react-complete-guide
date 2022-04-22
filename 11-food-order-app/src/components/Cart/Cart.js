f4a886672f6d4f67a090f6d0b44c406fb97057c0
unlimit1984 <unlimit_rmc@mail.ru>
2022-04-22 21:55:07 +0200

    [11-food-order] 140 Add a Modal via a Portal

--- 11-food-order-app/src/components/Cart/Cart.js
+++ 11-food-order-app/src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import classes from './Cart.module.css';
 
+import Modal from '../UI/Modal';
+
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -17,7 +19,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -27,7 +29,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
