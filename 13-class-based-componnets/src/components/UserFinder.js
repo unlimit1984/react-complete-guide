84c7da81993f94015bd264a24b4fd318e9322b87
unlimit1984 <unlimit_rmc@mail.ru>
2022-05-27 19:20:37 +0200

    [13-class-b-cmpts] 170 Add Error Boundaries

--- 13-class-based-componnets/src/components/UserFinder.js
+++ 13-class-based-componnets/src/components/UserFinder.js
@@ -1,6 +1,7 @@
 import { Fragment, useState, useEffect, Component } from 'react';
 
 import Users from './Users';
+import ErrorBoundary from './ErrorBoundary';
 import classes from './UserFinder.module.css';
 import UsersContext from '../store/users-context';
 
@@ -42,7 +43,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
